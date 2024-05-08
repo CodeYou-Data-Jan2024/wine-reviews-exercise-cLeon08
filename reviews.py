f6f8309f95e1848cca57d8f344fd7b5b3741f023
cLeon08 <92564127+cLeon08@users.noreply.github.com>
2024-05-08 19:50:13 -0400

    Initial commit

--- reviews.py
+++ reviews.py
@@ -1,2 +1,20 @@
-# add your code here
+import pandas as pd
 
+#read in data from given csv file
+wine_reviews = pd.read_csv("data/winemag-data-130k-v2.csv/winemag-data-130k-v2.csv")
+
+#create summary of data that contains: name, # of reviews, and average points for each unique country
+count = wine_reviews.groupby('country').size()
+points_mean = wine_reviews.groupby('country')['points'].mean()
+
+#creat dataframe
+reviews_sum = pd.DataFrame({
+    'country': count.index,
+    'count': count,
+    'points': points_mean.values.round(1)})
+
+#set index of dataframe
+reviews_sum.set_index('country', inplace=True)
+
+#write data to csv file
+reviews_sum.to_csv('data/reviews-per-country.csv')
